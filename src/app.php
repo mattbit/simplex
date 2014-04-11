fab034ef16abf4bc210ee5427f10701b6ab4c13b
Matteo <matthweb@gmail.com>
2014-04-11 19:43:18 +0200

    Clean routes

--- src/app.php
+++ src/app.php
@@ -6,10 +6,6 @@ use Symfony\Component\HttpFoundation\Response;
 
 $routes = new RouteCollection();
 
-$routes->add('hello', new Route('/hello/{name}', array('_controller' => 'render_template', 'name' => 'World')));
-$routes->add('bye', new Route('/bye/{name}'));
-
-
 $routes->add('leap_year', new Route('/is_leap_year/{year}', array(
 		'year' => null,
 		'_controller' => 'Calendar\\Controller\\LeapYearController::indexAction',
